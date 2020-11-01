f833570b388708b3765eb467e86bb70e0b9826cb
Tanvi-Goyal <goyaltanvi94@gmail.com>
2020-11-01 12:55:06 +0530

    Accessed user current locaation | Alert dialog for setting username added

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,7 +1,20 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.content.*
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -9,18 +22,161 @@ import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.utils.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private var currentNavController: LiveData<NavController>? = null
     private lateinit var binding : ActivityMainBinding
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var lastLocation: Location? = null
+
+    companion object {
+        private const val TAG = "LocationProvider"
+        private const val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (!checkPermissions()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions()
+            }
+        }
+        else {
+            getLastLocation()
+        }
+    }
+
+    private fun getLastLocation() {
+        fusedLocationClient?.lastLocation!!.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful && task.result != null) {
+                lastLocation = task.result
+                Log.wtf(TAG, "Latitude :" + (lastLocation)!!.latitude)
+                Log.wtf(TAG, "Longitude :" + (lastLocation)!!.longitude)
+
+                showDialog()
+            }
+            else {
+                Log.w(TAG, "getLastLocation:exception", task.exception)
+                showMessage("No location detected. Make sure location is enabled on the device.")
+            }
+        }
+    }
+
+    private fun showDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_username, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Set Username")
+        val  mAlertDialog = mBuilder.show()
+        mDialogView.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+            //get text from EditTexts of custom layout
+//            val name = mDialogView.text.toString()
+//            val email = mDialogView.dialogEmailEt.text.toString()
+//            val password = mDialogView.dialogPasswEt.text.toString()
+//            //set the input text in TextView
+//            mainInfoTv.setText("Name:"+ name +"\nEmail: "+ email +"\nPassword: "+ password)
+        }
+        //cancel button click of custom layout
+//        mDialogView.dialogCancelBtn.setOnClickListener {
+//            //dismiss dialog
+//            mAlertDialog.dismiss()
+//        }
+    }
+
+    private fun showMessage(string: String) {
+//        val container = findViewById<View>(R.id.linearLayout)
+//        if (container != null) {
+//            Toast.makeText(this@MainActivity, string, Toast.LENGTH_LONG).show()
+//        }
+    }
+
+    private fun showSnackbar(
+        mainTextStringId: String, actionStringId: String,
+        listener: View.OnClickListener
+    ) {
+        Toast.makeText(this@MainActivity, mainTextStringId, Toast.LENGTH_LONG).show()
+    }
+
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+            showSnackbar("Location permission is needed for core functionality", "Okay",
+                View.OnClickListener {
+                    startLocationPermissionRequest()
+                })
+        }
+        else {
+            Log.i(TAG, "Requesting permission")
+            startLocationPermissionRequest()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> {
+                    // If user interaction was interrupted, the permission request is cancelled and you
+                    // receive empty arrays.
+                    Log.i(TAG, "User interaction was cancelled.")
+                }
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                    // Permission granted.
+                    getLastLocation()
+                }
+                else -> {
+                    showSnackbar("Permission was denied", "Settings",
+                        View.OnClickListener {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts(
+                                "package",
+                                Build.DISPLAY, null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                    )
+                }
+            }
         }
     }
 
